23693864f283cc7699a6c26abd9b8a0c841c0e15
sj <sebastianthegreatful@gmail.com>
2020-04-19 02:17:26 +0700

    ExerciseRequest.id not nullable

--- app/src/main/java/dk/fitfit/mykoinapplication/domain/dto/ExerciseRequest.kt
+++ app/src/main/java/dk/fitfit/mykoinapplication/domain/dto/ExerciseRequest.kt
@@ -3,5 +3,5 @@ package dk.fitfit.mykoinapplication.domain.dto
 data class ExerciseRequest(
         val name: String,
         val description: String,
-        val id: Long = 0
+        val id: Long
 )
