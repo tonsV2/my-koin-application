585f54f6082f18503bddb58c2c9a0ed1caebeeb2
sj <sebastianthegreatful@gmail.com>
2020-04-19 07:34:07 +0700

    Replace ExerciseRepositoryImpl with ExerciseRepository

--- app/src/main/java/dk/fitfit/mykoinapplication/di/databaseModule.kt
+++ app/src/main/java/dk/fitfit/mykoinapplication/di/databaseModule.kt
@@ -2,12 +2,11 @@ package dk.fitfit.mykoinapplication.di
 
 import dk.fitfit.mykoinapplication.db.FitLogDatabase
 import dk.fitfit.mykoinapplication.db.repository.ExerciseRepository
-import dk.fitfit.mykoinapplication.db.repository.impl.ExerciseRepositoryImpl
 import org.koin.dsl.module
 
 @JvmField
 val databaseModule = module {
     single { FitLogDatabase.getDatabase(get()) }
     single { get<FitLogDatabase>().exerciseDao() }
-    single<ExerciseRepository> { ExerciseRepositoryImpl(get()) }
+    single { ExerciseRepository(get()) }
 }
